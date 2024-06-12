c2d275998af3ce99d790e70a74a8feae6c2496cc
No-face-117 <sayedmahamudalif@gmail.com>
2024-06-13 00:45:16 +0600

    Add files via upload

---
+++ Beginner_3.java
@@ -0,0 +1,24 @@
+public class Beginner_3 {
+
+    public static void main(String[] args) 
+    {
+    
+     double radius = 4.0;
+
+     double circumference = 2 * Math.PI * radius;
+     double area = Math.PI * radius * radius;
+
+     System.out.println("For a circle with a radius of " + radius + " units:");
+
+     System.out.println();
+
+     System.out.println("Circumference: " + circumference + " units.");
+
+     System.out.println();
+
+     System.out.println("Area: " + area + " square units.");
+
+
+    }
+}
+
