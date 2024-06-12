a39e5bf85558ff81f0cef01c684e866a6af89f83
No-face-117 <sayedmahamudalif@gmail.com>
2024-06-13 00:34:12 +0600

    Add files via upload

---
+++ Beginner_2.java
@@ -0,0 +1,121 @@
+public class Beginner_2 {
+
+    public static void main(String[] args) 
+    {
+    //2.1
+    System.out.println("2.1");
+    System.out.println();
+
+    int number_1 = 5;
+    System.out.println("number_1 = " + number_1);
+
+    System.out.println();
+
+    // 2.2
+    System.out.println("2.2");
+    System.out.println();
+
+    int number_2 = 35;
+    System.out.println("number_2 = " + number_2);
+
+    System.out.println();
+
+    int sum = number_1 + number_2;
+    System.out.println("sum = " + sum);
+
+    System.out.println();
+
+    // 2.3
+    System.out.println("2.3");
+    System.out.println();
+
+    int product = number_1 * number_2;
+    System.out.println("product = " + product);
+
+    System.out.println();
+
+    double division = number_2 / number_1;
+    System.out.println("division = " + division );
+
+    System.out.println();
+
+    // 2.4 
+    System.out.println("2.4");
+    System.out.println();
+
+    double number_1_1 = 5.777;
+    double number_2_2 = 35.777;
+
+    System.out.println("number_1_1 = " + number_1_1);
+    System.out.println();
+    System.out.println("number_2_2 = " + number_2_2);
+
+    System.out.println();
+
+    double sum_1 = number_1_1 + number_2_2;
+    System.out.println("Sum of double = " + sum_1);
+
+    System.out.println();
+
+    double product_1 = number_1_1 * number_2_2;
+    System.out.println("product of double = " + product_1);
+
+    System.out.println();
+
+    double division_1 = number_1_1 / number_2_2;
+    System.out.println("division of double = " + division_1);
+
+    System.out.println();
+
+    // 2.5
+    System.out.println("2.5");
+    System.out.println();
+
+    int number_1_1_1 = 5;
+    double number_2_2_2 = 35.777;
+
+    System.out.println("number_1_1 = " + number_1_1_1);
+    System.out.println();
+    System.out.println("number_2_2 = " + number_2_2_2);
+
+    System.out.println();
+
+    double sum_1_1 = number_1_1_1 + number_2_2_2;
+    System.out.println("Sum of double and  integer = " + sum_1_1);
+
+    System.out.println();
+
+    double product_1_1 = number_1_1_1 * number_2_2_2;
+    System.out.println("product of double and  integer = " + product_1_1);
+
+    System.out.println();
+
+    double division_1_1 = number_1_1_1 / number_2_2_2;
+    System.out.println("division of double and  integer = " + division_1_1);
+
+    System.out.println();
+
+    // 2.6
+    System.out.println("2.6");
+    System.out.println();
+
+    String number_11 = "5 appels ,";
+    System.out.println("number_11 = " + number_11);
+
+    System.out.println();
+
+    String number_22 = "35 banana";
+    System.out.println("number_22 = " + number_22);
+
+    System.out.println();
+
+    String sum_11 = number_11 + number_22;
+    System.out.println("sum of string = " + sum_11);
+
+    System.out.println();
+
+    System.out.println("Adding two or more String variables together only puts them one after the other")
+    
+    }
+}
+
