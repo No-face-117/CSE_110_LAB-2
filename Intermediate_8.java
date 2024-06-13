484380d88e1bf71a2a5e067d18b78dcae1183cc6
No-face-117 <sayedmahamudalif@gmail.com>
2024-06-13 16:08:37 +0600

    Add files via upload

---
+++ Intermediate_8.java
@@ -0,0 +1,22 @@
+public class Intermediate_8 {
+    public static void main(String[] args) 
+    { 
+
+    int a = 2;
+    System.out.println("a = " + a);
+    System.out.println();
+
+    int b = 5;
+    System.out.println("b = " + b);
+    System.out.println();
+
+    int c = 8;
+    System.out.println("c = " + c);
+    System.out.println();
+
+    double d = ((2 * b) * ((c - a) / 3)) + 7 ;
+    System.out.println("The value of d = " + d);
+       
+    }
+}
+
